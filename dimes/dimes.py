03f70d2beeffd10da7677c3fd35cfccc59309d82
unknown <nathan.oliver@bigladdersoftware.com>
2023-12-04 06:02:09 -0700

    Create finalize_plot function.

--- dimes/dimes.py
+++ dimes/dimes.py
@@ -17,9 +17,28 @@ class TimeSeriesPlot:
         self.title = ""
         self.time_values = time_values
         # self.subplots = []#: list(TimeSeriesData) = []
+    def finalize_plot(self):
+        if not self.is_finalized:
+            if not self.series:
+                raise Exception("No time series data provided.")
+            for time_series in self.series:
+                self.fig.add_trace(
+                    Scatter(
+                        x=self.time_values,
+                        y=time_series.data_values,
+                        name=time_series.name,
+                        mode="lines",
+                        visible='legendonly' if not time_series.is_visible else True,
+                        line=dict(
+                            color=time_series.color
+                        )),
+                    row=time_series.subplot_index,
+                    # row=1,
+                    col=1
+                    )
+            
+            self.is_finalized = True
+        
 
     def write_html_plot(self, path: Path):
-        self.figure.write_html(path)
-
-
-
+        self.finalize_plot()
