bdfac2d76e6951f09dc827261e498e4abe35bd2f
unknown <nathan.oliver@bigladdersoftware.com>
2023-12-05 09:29:11 -0700

    Add unit tests.

--- test/test_dimes.py
+++ test/test_dimes.py
@@ -1,6 +1,7 @@
 #pylint:disable=C0114
 
 from dimes import TimeSeriesPlot, TimeSeriesData
+import pytest
 
 def test_something():
     plot = TimeSeriesPlot([1,2,3,4,5])
@@ -8,3 +9,18 @@ def test_something():
     plot.add_time_series(TimeSeriesData([x**3 for x in plot.time_values]))
 
     plot.write_html_plot("output.html")
+
+def test_empty_plot():
+    
+    plot = TimeSeriesPlot([1,2,3,4,5])
+    
+    with pytest.raises(Exception):
+        plot.write_html_plot("output.html")
+
+def test_plot_twice():
+    plot = TimeSeriesPlot([1,2,3,4,5])
+    plot.add_time_series(TimeSeriesData([x**2 for x in plot.time_values]))
+    plot.add_time_series(TimeSeriesData([x**3 for x in plot.time_values]))
+
+    plot.write_html_plot("output.html")
+    plot.write_html_plot("output2.html")
