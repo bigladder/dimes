ce477e6cdf48cb36bf071cbd6212e5fd8152f91a
Nathan Oliver <nathan.oliver@bigladdersoftware.com>
2024-05-03 09:24:25 -0600

    Add title to plot.

--- dimes/timeseries.py
+++ dimes/timeseries.py
@@ -1,6 +1,7 @@
 """Module for plotting time series data."""
 
 from .common import DimensionalPlot, DisplayData
+from typing import Union
 
 
 class TimeSeriesData(DisplayData):
@@ -12,7 +13,8 @@ class TimeSeriesData(DisplayData):
 class TimeSeriesPlot(DimensionalPlot):
     """Time series plot."""
 
-    def __init__(self, time_values: list):
+    def __init__(self, time_values: list, plot_title: Union[str, None] = None):
         super().__init__(time_values)
         self.add_time_series = self.add_display_data
         self.time_values = self.x_axis_values
+        self.figure.layout["title"] = plot_title
