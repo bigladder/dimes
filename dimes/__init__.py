3da01d86dea4993cb6d8280bf6835ea4ddcf141a
Nathan Oliver <nathan.oliver@bigladdersoftware.com>
2024-05-06 12:53:47 -0600

    Import LegendProperties.

--- dimes/__init__.py
+++ dimes/__init__.py
@@ -1,5 +1,5 @@
 """dimes public interface"""
 
-from .common import LineProperties, DisplayData
+from .common import LegendProperties, LineProperties, DisplayData
 from .timeseries import TimeSeriesPlot, TimeSeriesData
 from .griddeddata import GridAxis, GridPointData, RegularGridData
